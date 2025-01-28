425b208bdbe6de8f9f1cb4709f447578d51e15a3
will-nasc <willian.silva@foursys.com.br>
2025-01-27 22:34:20 -0300

    criando a azure function

--- fnPostDataStorage/Function1.cs
+++ fnPostDataStorage/Function1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,22 +12,55 @@ namespace fnPostDataStorage
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            _logger = logger;
+        }
 
-            string name = req.Query["name"];
+        [Function("dataStorage")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            _logger.LogInformation("Subindo imagem para o hd!");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            if (!req.Headers.TryGetValue("file-type", out var fileTypeHeader))
+            {
+                return new BadRequestObjectResult("O cabeçalho 'file-type' é obrigatório");
+            }
+
+            var fileType = fileTypeHeader.ToString();
+            var form = await req.ReadFormAsync();
+            var file = form.Files["file"];
+
+            if (file == null || file.Length == 0)
+            {
+                return new BadRequestObjectResult("O arquivo não foi enviado");
+            }
+
+            string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string containerName = fileType;
+            BlobClient blobClient = new BlobClient(connectionString, containerName, file.FileName);
+            BlobContainerClient containerClient new BlobContainerClient(connectionString, containerName);
+
+            await containerClient.CreateIfNotExistsAsync();
+            await containerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer);
+
+            string blobName = file.FileName;
+            var blob = containerClient.GetBlobClient(blobName);
+            using (var stream = file.OpenReadStream())
+            {
+                await blob.UploadAsync(stream, true);
+            }
+
+            _logger.LogInformation($"Arquivo {file.FileName} armazenado com sucesso");
+
+            return new OkObjectResult(new
+            {
+                Message "Arquivo armazęnado com sucesso",
+                BlobUri = blob.Uri
+            });
         }
     }
 }
---
+++ fnPostDataStorage/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Service.Configure<KestrelServerOptions>(options =>
+{
+    options.Limits.MaxRequestBodySize = 104857600; //1024 * 1024 * 100;
+});
+
+builder.Build().Run();
